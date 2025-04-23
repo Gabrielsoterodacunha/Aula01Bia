881d541aec0e422858ca7ccd12f4d94256d5f3a4
Gabriel hub <165857910+Gabrielsoterodacunha@users.noreply.github.com>
2025-04-22 21:10:25 -0300

    Video 1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula Complementar - UNG</title>
+    <style>
+        #principal {
+            width: 500px;
+            height: 500px;
+            position: relative;
+            margin: auto;
+            background-color: #af8; /* Cor ajustada */
+            border: 2px solid #555; /* Borda opcional */
+            padding: 20px;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div id="principal">
+        <form action="" method="POST">
+            <label for="nome">Nome:</label><br/>
+            <input type="text" name="cxnome" id="nome"><br/><br/>
+            <label for="idade">Idade:</label><br/>
+            <input type="text" name="cxidade" id="idade"><br/><br/>
+            <input type="submit" name="Gravar"><br/>
+        </form>
+    </div>
+</body>
+</html>
